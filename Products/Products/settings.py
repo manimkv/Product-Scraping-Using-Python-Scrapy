6adc56057c1525e24afb31ad0eba5ea144323906
manimkv <manikandanv3131@gmail.com>
2015-03-09 20:17:21 +0530

    http://www.chennaibasket.com scraped

---
+++ Products/Products/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for Products project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'Products'
+
+SPIDER_MODULES = ['Products.spiders']
+NEWSPIDER_MODULE = 'Products.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'Products (+http://www.yourdomain.com)'
---
+++ Products/Products/spiders/chennaibasket.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from scrapy import log
+from scrapy.spider import Spider
+from scrapy.selector import Selector
+from scrapy.http.request import Request
+from urlparse import urljoin
+# from lib.mongo_helpers import db
+from datetime import date
+import csv
+
+class ChennaibasketSpider(Spider):
+    name = "chennaibasket"
+    allowed_domains = ["chennaibasket.com"]
+    start_urls = (
+        'http://www.chennaibasket.com/',
+    )   
+
+    def parse(self, response):
+        sel = Selector(response)
+        self.today = date.today().strftime('%d-%m-%Y')
+        xp = lambda x: sel.xpath(x)
+        
+        for x in xp("//*[@class='tlli mkids']"):
+            fetch_url = x.re('<a class="tll" href="(.*)"')
+            yield Request(fetch_url[0], callback=self.parse_url)
+
+
+    def parse_url(self, response):
+        sel = Selector(response)
+        xp = lambda x: sel.xpath(x)
+        rest_pages = list(set(xp("//*[@class='links']//@href").extract()))
+        rest_pages.append('%s?page=1' % response.url)
+        self.category = open('%s.csv' % response.url.split('/')[-1], 'wb')
+        self.csv_writer = csv.writer(self.category)
+        self.csv_writer.writerow(['date', 'product with quantity', 'price'])
+
+        for page_url in rest_pages:
+            yield Request(page_url, callback=self.parse_page)            
+
+
+    def parse_page(self, response):
+        sel = Selector(response)
+        xp = lambda x: sel.xpath(x)
+
+        for x in xp('//*[@class="three mobile-two columns"]'):
+            try:
+                price = x.xpath('.//*[@class="price-new"]//text()').extract()[0]
+            except IndexError:
+                price = x.xpath('.//*[@class="price"]//text()').extract()[0].replace('\n', '').strip()
+            product = x.xpath('.//*[@class="name"]//text()').extract()[0]
+            try:
+                self.csv_writer.writerow([self.today, product, price])
+            except UnicodeEncodeError:
+                self.csv_writer.writerow([self.today, product.encode('ascii', 'ignore'), price])
+        
+        return
+                 
+
+
+
